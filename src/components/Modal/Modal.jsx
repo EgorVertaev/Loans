6628b9e2a7179eeb1d4b8c372d5082ed2c475ab9
EgorVertaev <eholife123@gmail.com>
2022-01-16 18:43:48 +0200

    added style for Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,18 +10,20 @@ function Modal({data, active, setActive }) {
     <div>
       <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
         <form className={active ? "modal__content active" : "modal__content"} onClick={(e) => e.stopPropagation()}>
-          <h3 className="modal__title">Invest in Loan</h3>
-          <p>{title}</p>
-          <p>Amount avalible: £ {available}</p>
-          <p>Loan ends in: {getDays(term_remaining)} days</p>
-          <label htmlFor="investAmount">Investment amount (£)</label>
-          <input
-            name="investAmount"
-            id="investAmount"
-            type="number"
-            placeholder="1,000"
-          />
-          <button type="submit">invest</button>
+          <a href="#" className="modal__title" onClick={(e) => e.preventDefault()}>{title}</a>
+          <p className="modal__avalible">Amount avalible: £ {available}</p>
+          <p className="modal__days">Loan ends in: {getDays(term_remaining)} days</p>
+          <label className="modal__amount" htmlFor="investAmount">Investment amount (£)</label>
+          <div className="modal__inner">
+            <input
+              className="modal__input"
+              name="investAmount"
+              id="investAmount"
+              type="number"
+              placeholder="1,000"
+            />
+            <button className="modal__btn" type="submit">invest</button>
+          </div>
         </form>
       </div>
     </div>
