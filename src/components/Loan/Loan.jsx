1c9cb9699cb862572febf15a1862c8491550724e
EgorVertaev <eholife123@gmail.com>
2022-01-16 15:38:22 +0200

    added reset style and total amount

--- src/components/Loan/Loan.jsx
+++ src/components/Loan/Loan.jsx
@@ -1,12 +1,12 @@
 import "./Loan.css"
 
-function Loan() {
+function Loan({data, id}) {
 
     return (
         <div className="loan">
             <div className="loan__content">
-                <h2 className="loan__title"></h2>
-                <p className="loan__info"></p>
+                <h2 className="loan__title">{data.title}</h2>
+                <p className="loan__info">amount: {data.amount} ltv: {data.ltv} annualised return: {data.annualised_return}</p>
             </div>
             <div className="loan__button">
                 <button className="loan__btn" type="button">
--- src/components/Loans.jsx
+++ src/components/Loans.jsx
@@ -1,12 +1,21 @@
 import "./Loans.css"
 import Loan from "./Loan/Loan"
+import { useState } from "react"
+const { loans } = require('./../api/current-loans.json')
 
 function Loans() {
+    const [loansData, setLoansData] = useState(loans)
 
+    const totalAmount = loansData.reduce((total, loan) => {
+        let avalibleNum = Number(loan.available.replace(',',''))
+        return total + avalibleNum;
+    }, 0)
+    
     return (
         <div className="loans">
             <h1 className="loans__title">Current Loans</h1>
-            <Loan/>
+            {loansData.map((loan) => <Loan data={loan} id={loan.id}/>)}
+            <p>Total amount avalible for investments: {totalAmount}</p>
         </div>
     )
 }
